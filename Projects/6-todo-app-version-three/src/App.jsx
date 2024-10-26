ca43b80c2bcd7fc38eac994cd92cd7840410b972
Shakti <mehershakti1020@gmail.com>
2024-10-26 21:22:55 +0530

    todo app update version 3.0

--- Projects/6-todo-app-version-three/src/App.jsx
+++ Projects/6-todo-app-version-three/src/App.jsx
@@ -1,32 +1,38 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./WelcomeMessage";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const todoItems = [
-    {
-      name: "Buy Milk",
-      dueDate: "04/10/2023",
-    },
-    {
-      name: "Go to College",
-      dueDate: "04/10/2023",
-    },
-    {
-      name: "Like this video",
-      dueDate: "right now",
-    },
-  ];
+  const initialTodoItems = [];
 
-  
+  let [todoItems, setTodoItems] = useState(initialTodoItems);
 
+  const handleNewItem = (itemName, itemDate) => {
+    console.log(`New Item added: ${itemName} Date: ${itemDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      {
+        name: itemName,
+        dueDate: itemDate,
+      },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
 
   return (
     <center className="todo-container">
       <AppName />
-      <AddTodo />
-      <TodoItems todoItems={todoItems}/>
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage />}
+      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem} />
     </center>
   );
 }
--- Projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,15 +1,46 @@
-function AddTodo() {
+import { useState } from "react";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [dueDate, setDueDate] = useState();
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+
+  };
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, dueDate);    
+    setTodoName("");
+    setDueDate("");
+  };
+
   return (
-    <div class="container">
+    <div class="container text-center">
       <div class="row kg-row">
         <div class="col-6">
-          <input type="text" placeholder="Enter Text Here" />
+          <input
+            type="text"
+            value={todoName}
+            placeholder="Enter Text Here"
+            onChange={handleNameChange}
+          />
         </div>
         <div class="col-4">
-          <input type="date" />
+          <input type="date"
+          value={dueDate} 
+          onChange={handleDateChange} />
         </div>
         <div class="col-2">
-          <button type="button" class="btn btn-success kg-button">
+          <button
+            type="button"
+            class="btn btn-success kg-button"
+            onClick={handleAddButtonClicked}
+          >
             Add
           </button>
         </div>
