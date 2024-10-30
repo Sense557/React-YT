f45c3b4a2fedd232e7a1dec3f15b7c4d12bfab75
Shakti <mehershakti1020@gmail.com>
2024-10-31 00:05:10 +0530

    ok

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () => {
-  return <>
-    <Post />
-    <Post />
-    <Post />
-  </>
-}
+  const { postList } = useContext(PostListData);
+  console.log(postList);
+  return (
+    <>
+      {postList.map((post) => {
+        <Post />;
+      })}
+    </>
+  );
+};
 
 export default PostList;
--- Projects/7-social-media/src/store/post-list-store.jsx
+++ Projects/7-social-media/src/store/post-list-store.jsx
@@ -1,9 +1,56 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({});
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
 
-const PostListProvider = ({children}) => {
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>
-}
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  let [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POSTLIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider
+      value={{
+        postList: postList,
+        addPost: addPost,
+        deletePost: deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POSTLIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hii Friends, I am going to Mumbai for my vacation. Hope to enjoy a lot. Peace Out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["Vacation", "Mumbai", "Enjoying"],
+  },
+
+  {
+    id: "2",
+    title: "Pass ho gaya",
+    body: "4 saal ki masti k baad bhi ho gaye hain pass. Hard to believe",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelieveable"],
+  },
+];
 
 export default PostListProvider;
