26faaed58067654e0334c4d81a5750668945d233
Shakti <mehershakti1020@gmail.com>
2024-11-07 22:34:52 +0530

    ok

--- 52-redux/Counter-React-Redux/52-Redux/src/App.jsx
+++ 52-redux/Counter-React-Redux/52-Redux/src/App.jsx
@@ -2,34 +2,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
+import Container from "./components/Container";
+import Controls from "./components/Controls";
 
 function App() {
   return (
-    <>
-      <div className="px-4 pt-5 my-5 text-center border-bottom">
+    <center className="px-4 pt-5 my-5 text-center">
+      <Container>
         <Header />
         <div className="col-lg-6 mx-auto">
           <DisplayCounter />
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg px-4 me-sm-3"
-            >
-              Primary button
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
-            >
-              Secondary
-            </button>
-          </div>
+
+          <Controls />
         </div>
-        <div className="overflow-hidden" style={{ maxHeight: "30vh" }}>
-          <div className="container px-5"></div>
-        </div>
-      </div>
-    </>
+      </Container>
+    </center>
   );
 }
 
--- 52-redux/Counter-React-Redux/52-Redux/src/components/DisplayCounter.jsx
+++ 52-redux/Counter-React-Redux/52-Redux/src/components/DisplayCounter.jsx
@@ -1,12 +1,5 @@
 const DisplayCounter = () => {
-  return (
-    <p className="lead mb-4">
-      Quickly design and customize responsive mobile-first sites with Bootstrap,
-      the world’s most popular front-end open source toolkit, featuring Sass
-      variables and mixins, responsive grid system, extensive prebuilt
-      components, and powerful JavaScript plugins.
-    </p>
-  );
+  return <p className="lead mb-4">Counter Current Value</p>;
 };
 
 export default DisplayCounter;
