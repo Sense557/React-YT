07b3086a7c4852f8d5eb0fd5e76a599971d5c2a0
Shakti <mehershakti1020@gmail.com>
2024-11-09 16:37:25 +0530

    issue why app crashed

--- 52-redux/3-Counter-React-Redux-Toolkit/src/components/Controls.jsx
+++ 52-redux/3-Counter-React-Redux-Toolkit/src/components/Controls.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
-import { counterActions } from "../store";
+import { counterActions, privacyActions } from "../store";
 
 const Controls = () => {
   const dispatch = useDispatch();
@@ -15,26 +15,26 @@ const Controls = () => {
   };
 
   const handlePrivacyToggle = () => {
-    dispatch({ type: "PRIVACY_TOGGLE" });
+    dispatch(privacyActions.toggle());
   };
 
   const handleAdd = () => {
-    dispatch({
-      type: "ADD",
-      payload: {
+    dispatch(
+      counterActions.add({
         num: inputElement.current.value,
-      },
-    });
+      })
+    );
+
     inputElement.current.value = "";
   };
 
   const handleSubstract = () => {
-    dispatch({
-      type: "SUBSTRACT",
-      payload: {
+    dispatch(
+      counterActions.substract({
         num: inputElement.current.value,
-      },
-    });
+      })
+    );
+
     inputElement.current.value = "";
   };
 
--- 52-redux/3-Counter-React-Redux-Toolkit/src/store/index.js
+++ 52-redux/3-Counter-React-Redux-Toolkit/src/store/index.js
@@ -1,56 +1,14 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: {counterVal: 0},
-  reducers: {
-    increment: (state) => {
-      state.counterVal++;
-    },
-    decrement: (state) => {
-      state.counterVal--;
-    },
-    add: (state, action) => {
-      console.log(state, action);
-    },
-    substract: (state, action) => {
-      console.log(state, action);
-    }
-  }
-})
-
-
+import counterSlice from "./counter";
+import privacySlice from "./privacy";
 
 
 const counterStore = configureStore({reducer: {
-  counter: counterSlice.reducer
+  counter: counterSlice.reducer,
+  privacy: privacySlice.reducer
 }});
 
-export const counterActions = counterSlice.actions;
+
+
 export default counterStore;
 
-
-/*
-const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false,
-}
-
-
-const counterReducer = (store = INITIAL_VALUE, action) => {
-
-  if(action.type === 'INCREMENT') {
-    return {...store, counter: store.counter + 1};
-  }else if(action.type === 'DECREMENT') {
-    return {...store, counter: store.counter - 1};
-  }else if(action.type === 'ADD') {
-    return {...store, counter: store.counter + Number(action.payload.num)};
-  }else if(action.type === 'SUBSTRACT') {
-    return {...store, counter: store.counter - Number(action.payload.num)};
-  }else if(action.type === 'PRIVACY_TOGGLE') {
-    return {...store, privacy: !store.privacy};
-  }
-
-  return store;
-}
-*/
