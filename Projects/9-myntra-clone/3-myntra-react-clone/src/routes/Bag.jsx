5c21bc9e80de7d60e75033105034ead733a42495
Shakti <mehershakti1020@gmail.com>
2024-11-10 20:19:32 +0530

    ok

--- Projects/9-myntra-clone/3-myntra-react-clone/src/routes/Bag.jsx
+++ Projects/9-myntra-clone/3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,13 +1,30 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import BagItem from "../components/BagItem";
+import BagSummary from "../components/BagSummary";
 
 const Bag = () => {
+  const item = {
+    id: "001",
+    image: "images/1.jpg",
+    company: "Carlton London",
+    item_name: "Rhodium-Plated CZ Floral Studs",
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    return_period: 14,
+    delivery_date: "10 Oct 2023",
+    rating: {
+      stars: 4.5,
+      count: 1400,
+    },
+  };
   return (
     <>
       <main>
         <div className="bag-page">
-          <div className="items-container"></div>
-          <div className="bag-summary"></div>
+          <div className="items-container">
+            <BagItem item={item} />
+          </div>
+          <BagSummary />
         </div>
       </main>
     </>
