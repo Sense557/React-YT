249aa37d0a5517fc7206112f697aafeaf435dd81
Shakti <mehershakti1020@gmail.com>
2024-11-10 12:10:30 +0530

    ok but issue is not rendering the items in home

--- Projects/9-myntra-clone/3-myntra-react-clone/src/components/Header.jsx
+++ Projects/9-myntra-clone/3-myntra-react-clone/src/components/Header.jsx
@@ -1,19 +1,20 @@
 import { IoBagHandle } from "react-icons/io5";
 import { IoPersonSharp } from "react-icons/io5";
 import { FaHeart } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <header>
         <div className="logo_container">
-          <a href="#">
+          <Link to="/">
             <img
               className="myntra_home"
               src="images/myntra_logo.webp"
               alt="Myntra Home"
             />
-          </a>
+          </Link>
         </div>
         <nav className="nav_bar">
           <a href="#">Men</a>
@@ -45,12 +46,12 @@ const Header = () => {
             <span className="action_name">Wishlist</span>
           </div>
 
-          <a className="action_container" href="pages/bag.html">
+          <Link className="action_container" to="/bag">
             <IoBagHandle />
 
             <span className="action_name">Bag</span>
             <span className="bag-item-count">0</span>
-          </a>
+          </Link>
         </div>
       </header>
     </>
