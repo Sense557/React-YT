4855eb20b6859768f85604581dc84016039be303
Shakti <mehershakti1020@gmail.com>
2024-11-11 23:09:38 +0530

    ok

--- Projects/9-myntra-clone/3-myntra-react-clone/src/components/FetchItems.jsx
+++ Projects/9-myntra-clone/3-myntra-react-clone/src/components/FetchItems.jsx
@@ -1,33 +1,36 @@
-import { useSelector } from "react-redux";
-
-
-useEffect(() => {
-
-  const controller = new AbortController();
-  const signal = controller.signal;
-
-  setFetching(true);
-  fetch("https://dummyjson.com/posts", { signal })
-    .then((res) => res.json())
-    .then((data) => {
-      addInitialPosts(data.posts);
-      setFetching(false);
-    });
-  return () => {
-    console.log("Cleaning Up useEffect");
-    controller.abort();
-  };
-}, []);
-
+import { useDispatch, useSelector } from "react-redux";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
-  return <>
-    <div>
-      Fetch Done: {fetchStatus.fetchDone}
-      Currently Fetching: {fetchStatus.currentlyFetching}
-    </div>
-  </>;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (fetchStatus.fetchDone) return;
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      });
+    return () => {
+      console.log("Cleaning Up useEffect");
+      controller.abort();
+    };
+  }, [fetchStatus]);
+
+  return (
+    <>
+      <div>
+        Fetch Done: {fetchStatus.fetchDone}
+        Currently Fetching: {fetchStatus.currentlyFetching}
+      </div>
+    </>
+  );
 };
 
 export default FetchItems;
