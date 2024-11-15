8d433d4b690d53643e0859fd88094a7e7e943efe
Shakti <mehershakti1020@gmail.com>
2024-11-15 19:29:25 +0530

    Problems solved

--- Projects/9-myntra-clone/3-myntra-react-clone/src/components/FetchItems.jsx
+++ Projects/9-myntra-clone/3-myntra-react-clone/src/components/FetchItems.jsx
@@ -4,7 +4,6 @@ import { itemsActions } from "../store/itemsSlice";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,10 +13,15 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     fetch("http://localhost:8080/items", { signal })
-      .then((res) => res.json())
-      .then((items) => {
-        dispatch(itemsActions.addInitialItems(items[0]));
-      });
+    .then((res) => res.json())
+    .then((response) => {
+      console.log("Fetched Items", response.items); // Log the array inside the response object
+      dispatch(itemsActions.addInitialItems(response.items)); // Dispatch only the array part (response.items)
+    })
+    .catch((error) => {
+      console.error("Error fetching items:", error);
+    });
+  
     return () => {
       controller.abort();
     };
--- Projects/9-myntra-clone/3-myntra-react-clone/src/routes/Home.jsx
+++ Projects/9-myntra-clone/3-myntra-react-clone/src/routes/Home.jsx
@@ -3,17 +3,22 @@ import { useSelector } from "react-redux";
 
 const Home = () => {
   const items = useSelector((store) => store.items);
-  console.log("Got Items", items);
+  console.log("Got Items", items);  // You should now see the actual array of items
 
   return (
     <main>
       <div className="items-container">
-        {items.map((item) => (
-          <HomeItem key={item.id} item={item} />
-        ))}
+        {items.length > 0 ? (
+          items.map((item) => (
+            <HomeItem key={item.id} item={item} />
+          ))
+        ) : (
+          <p>No items available</p>
+        )}
       </div>
     </main>
   );
 };
 
-export default Home;
+
+export default Home; // This ensures Home is a default export
